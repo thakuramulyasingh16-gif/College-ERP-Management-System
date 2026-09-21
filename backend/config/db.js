15b6d2e4ec43f17e3a0f6cae84b3de80f002096a
Amulya Pratap Singh <thakuramulyasingh16@gmail.com>
2026-09-21 21:50:14 +0530

    Change SSL setting to not reject unauthorized certificates

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: true,
+    rejectUnauthorized: false,
   },
   waitForConnections: true,
   connectionLimit: 10,
