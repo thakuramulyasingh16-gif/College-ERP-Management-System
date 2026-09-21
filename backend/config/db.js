e6ed06a2335d0f19055b725741ba54bd1565406c
Amulya Pratap Singh <thakuramulyasingh16@gmail.com>
2026-09-21 22:05:52 +0530

    Add connectTimeout to database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectTimeout: 20000,
   ssl: {
     rejectUnauthorized: false,
   },
