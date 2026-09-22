55cff1b5cbabd69cf917a113bf4ef498d6244537
Amulya Pratap Singh <thakuramulyasingh16@gmail.com>
2026-09-22 12:51:20 +0530

    Enable SSL certificate verification

--- backend/config/db.js
+++ backend/config/db.js
@@ -15,7 +15,7 @@ const pool = mysql.createPool({
   connectTimeout: 20000,
   ssl: {
     ca: fs.existsSync(caCertPath) ? fs.readFileSync(caCertPath) : undefined,
-    rejectUnauthorized: false,
+    rejectUnauthorized: true,
   },
   waitForConnections: true,
   connectionLimit: 10,
